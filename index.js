293b00dafeaee99c4238dd39a750bb4b97124b4c
lowlyHero <e.nunn1009@gmail.com>
2019-06-17 17:03:48 -0400

    Got server up and running

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const server = express();
+
+const db = require('./data/db');
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('<h1>Hello Pale Blue Dot</h1>');
+})
+
+server.listen(4000, () => {
+    console.log('\n*** Server is running on port 4000 ***\n');
+})
